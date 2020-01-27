dcd1d9fd2dcf55fb608922419b01e465e953dc79
Femkant <k4rstrom@hotmail.com>
2020-01-27 16:54:42 +0100

    testing still

--- work/report/js/schools.js
+++ work/report/js/schools.js
@@ -11,4 +11,27 @@
         });
 
     console.log('JSON fetched');
+
+    updateTable(tableId, jsonData);
+    
 };
+
+function updateTable(tableId, jsonData){
+
+    var tableHTML = "<tr>";
+    for (var headers in jsonData[0]) {
+      tableHTML += "<th>" + headers + "</th>";
+    }
+    tableHTML += "</tr>";
+  
+    for (var eachItem in jsonData) {
+      tableHTML += "<tr>";
+      var dataObj = jsonData[eachItem];
+      for (var eachValue in dataObj){
+        tableHTML += "<td>" + dataObj[eachValue] + "</td>";
+      }
+      tableHTML += "</tr>";
+    }
+  
+    document.getElementById(tableId).innerHTML = tableHTML;
+  }
