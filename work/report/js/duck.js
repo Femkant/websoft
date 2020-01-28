e3e462bf314be6609d50a20299d408aa33abd8fb
Femkant <k4rstrom@hotmail.com>
2020-01-28 16:18:03 +0100

    task 3 completed

--- work/report/js/duck.js
+++ work/report/js/duck.js
@@ -1,16 +1,55 @@
 "use strict";
 
-(function () {
+var element = document.getElementById("duck");
+var counter = 0;
+var duckState = true;
 
-    var element = document.getElementById("duck");
+
+(function () {
+    element.style.position = 'absolute';
+    element.style.top = Math.floor(Math.random()*90+5)+'%';
+    element.style.left = Math.floor(Math.random()*90+5)+'%';
+
+    window.setInterval(switchPos, 2000);
 
     element.addEventListener("click", function () {
-        console.log("Duck clicked!");
+        counter = counter+1;
+        element.style.position = 'absolute';
+        element.style.top = Math.floor(Math.random()*90+5)+'%';
+        element.style.left = Math.floor(Math.random()*90+5)+'%';
+
+
+        var score = document.getElementById("score");
+        score.textContent = "Ducks shot: " + counter;
+
+        console.log(counter);
     });
 
     element.addEventListener("mouseover", function () {
-        element.style.left = element.offsetLeft + 20 + "px";
-        console.log("Mouse over!");
     });
-
+    
 })();
+
+
+function switchPos() {
+    element.style.position = 'absolute';
+    element.style.top = Math.floor(Math.random()*90+5)+'%';
+    element.style.left = Math.floor(Math.random()*90+5)+'%';
+}
+
+function switchDuckState() {
+    var btn = document.getElementById("duckBtn");
+    var duck = document.getElementById("duck");
+    if (duckState == true) {
+        duckState = false;
+        btn.textContent = "Start duck!"
+        duck.hidden=true;
+    }else {
+        duckState = true;
+        btn.textContent = "Stop duck!"
+        duck.hidden=false;
+        
+    }
+}
+
+
